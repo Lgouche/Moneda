f8d50a0a7cd18347b3a2a9b1ed8bf47f226ba8eb
Lgouche <estipion@gmail.com>
2024-03-06 13:27:30 +0100

    1.1v
    Continuamos con la creacion del podometro

--- Podometro/App.js
+++ Podometro/App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Platform, Text, View, Button, SafeAreaView } from 'react-native';
+import { useState } from 'react';
 
+const colors = ["#F7DC6F","#A2D9CE","#D7BDE2"];
 export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25*60);
+  const [currentTime, setCurrentTime]= useState("Pomo"|"Short"|"Break");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    //Este elemento permite en los ios poder poner los objetos son tocar el barra superior
+<SafeAreaView styles={styles.container}>
+    <View style={{paddingTop: Platform.OS === "android" && 30}}>
+      <Text style={styles.text}>Podometro</Text>
       <StatusBar style="auto" />
     </View>
+</SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  text: {
+    fontSize: 32,
+    fontWeight:"bold"
+  }
 });
---
+++ Podometro/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import { View,Text,TouchableOpacity,StyleSheet } from "react-native"
+
+
+export default function Header(){
+return(
+    <View>
+    </View>
+)
+}
